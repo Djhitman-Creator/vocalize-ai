311518ec9e30ca86499d5077c14a9444be10e67d
Djhitman-Creator <jboyte72@gmail.com>
2026-01-10 23:38:08 -0600

    Temporarily disable chatbot to test dashboard

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -1,24 +1,10 @@
 import '../styles/globals.css';
-import { ThemeProvider, useTheme } from '../context/ThemeContext';
-import dynamic from 'next/dynamic';
-
-// Dynamically import ChatBot with no SSR to prevent hydration issues
-const ChatBot = dynamic(() => import('../components/ChatBot'), {
-  ssr: false,
-  loading: () => null
-});
-
-// Wrapper component to safely access theme context
-function ChatBotWrapper() {
-  const { isDark } = useTheme();
-  return <ChatBot isDark={isDark} />;
-}
+import { ThemeProvider } from '../context/ThemeContext';
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider>
       <Component {...pageProps} />
-      <ChatBotWrapper />
     </ThemeProvider>
   );
 }
